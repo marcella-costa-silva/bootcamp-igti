4f7bc513e93b9ba8dbb107084ca6f90749f93067
Marcella <marcellatsangos@gmail.com>
2020-05-19 14:45:23 -0300

    State variables

--- 01-Javascript/countries/js/script.js
+++ 01-Javascript/countries/js/script.js
@@ -1,3 +1,18 @@
+// Variáveis de estado
+let tabCountries = null
+let tabFavorites = null
+
+let allCountries = []
+let favoriteCountries = []
+
+let countCountries = 0
+let countFavorites = 0
+
+let totalPopulation = 0
+let totalPopulationFavorites = 0
+
+let numberFormat = null
+
 window.addEventListener('load', () => {
   
 })
