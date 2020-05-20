0d63d68568846ae1d65c9b75400c983c81d66b17
Marcella <marcellatsangos@gmail.com>
2020-05-20 09:53:11 -0300

    fetchUsers function

---
+++ 01-Javascript/users-search/js/script.js
@@ -0,0 +1,43 @@
+// Variáveis de Estado
+let tabUsers = null
+let tabStatistics = null
+
+let allUsers = []
+
+let countUsers = 0 // Usuários encontrados
+let countMaleGender = 0
+let countFemaleGender = 0
+
+let agesSum = 0
+let agesAverage = 0
+
+let numberFormat = null
+
+window.addEventListener('load', () => {
+
+  fetchUsers()
+})
+
+async function fetchUsers() {
+  const url = await 'https://randomuser.me/api/?results=100'
+  const res = await fetch(url)
+  const json = await res.json()
+
+  allUsers = json.results.map(user => {
+    const { name, picture, dob, gender } = user
+
+    return {
+      name: name.first + ' ' + name.last,
+      picture: picture.thumbnail,
+      age: dob.age,
+      gender
+    }
+  })
+  
+  console.log(allUsers)
+  render()
+}
+
+function render() {
+  
+}
